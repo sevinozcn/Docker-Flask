5062a5ca877ed1c60a014949ee1cfd693686f56c
sevinozcn <sevinozcan8@gmail.com>
2024-01-15 15:42:44 +0000

    docker eklendi

--- ornek.py
+++ ornek.py
@@ -56,5 +56,5 @@ api.add_resource(Cities, '/cities')
 api.add_resource(Name, '/isim/<string:name>')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=6767)
+    app.run(host="0.0.0.0", port=5000)
     app.run()
